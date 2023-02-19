d4e3b1b14bf4bcda4f385424645db9163e3208bf
Alex <olekhyzhniak@gmail.com>
2023-02-19 16:09:21 +0200

    modal window scrolling

--- js/main.js
+++ js/main.js
@@ -17,6 +17,32 @@ const smoothScroll = () => {
 }
 smoothScroll();
 
+// LIBRARY SECTION (modal window)
+
+// const scrollController = {
+//     scrollPosition: 0,
+//     disabledScroll() {
+//         scrollController.scrollPosition = window.scrollY;
+//         document.body.style.cssText = `
+//       overflow: hidden;
+//       position: fixed;
+//       top: -${scrollController.scrollPosition}px;
+//       left: 0;
+//       height: 100vh;
+//       width: 100vw;
+//       padding-right: ${window.innerWidth - document.body.offsetWidth}px
+//     `;
+//         document.documentElement.style.scrollBehavior = 'unset';
+//     },
+//     enabledScroll() {
+//         document.body.style.cssText = '';
+//         window.scroll({
+//             top: scrollController.scrollPosition
+//         })
+//         document.documentElement.style.scrollBehavior = '';
+//     },
+// }
+
 // LIBRARY SECTION
 const librarySection = () => {
     const hideCardsFirstRow = document.getElementById('hide-cards-first-row');
