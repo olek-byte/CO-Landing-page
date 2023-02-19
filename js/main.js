678ac35081ce60c4918f85214a7bed2da472fd37
Alex <olekhyzhniak@gmail.com>
2023-02-19 16:24:31 +0200

    modal window scrolling2

--- js/main.js
+++ js/main.js
@@ -19,29 +19,29 @@ smoothScroll();
 
 // LIBRARY SECTION (modal window)
 
-// const scrollController = {
-//     scrollPosition: 0,
-//     disabledScroll() {
-//         scrollController.scrollPosition = window.scrollY;
-//         document.body.style.cssText = `
-//       overflow: hidden;
-//       position: fixed;
-//       top: -${scrollController.scrollPosition}px;
-//       left: 0;
-//       height: 100vh;
-//       width: 100vw;
-//       padding-right: ${window.innerWidth - document.body.offsetWidth}px
-//     `;
-//         document.documentElement.style.scrollBehavior = 'unset';
-//     },
-//     enabledScroll() {
-//         document.body.style.cssText = '';
-//         window.scroll({
-//             top: scrollController.scrollPosition
-//         })
-//         document.documentElement.style.scrollBehavior = '';
-//     },
-// }
+const scrollController = {
+    scrollPosition: 0,
+    disabledScroll() {
+        scrollController.scrollPosition = window.scrollY;
+        document.body.style.cssText = `
+      overflow: hidden;
+      position: fixed;
+      top: -${scrollController.scrollPosition}px;
+      left: 0;
+      height: 100vh;
+      width: 100vw;
+      padding-right: ${window.innerWidth - document.body.offsetWidth}px
+    `;
+        document.documentElement.style.scrollBehavior = 'unset';
+    },
+    enabledScroll() {
+        document.body.style.cssText = '';
+        window.scroll({
+            top: scrollController.scrollPosition
+        })
+        document.documentElement.style.scrollBehavior = '';
+    },
+}
 
 // LIBRARY SECTION
 const librarySection = () => {
