4c476b6bd5d74fd396f87eb8066a937c5dedca62
Alex <olekhyzhniak@gmail.com>
2023-11-23 14:04:11 +0200

    connection to pdf.js library

--- js/main.js
+++ js/main.js
@@ -1,3 +1,76 @@
+//PDF Rendering page
+initPDFViewer = () => {
+  $("#pdfViewerDiv").html("");
+  pdfjsLib
+    .getDocument("Offering-Memorandum-For-Sale.pdf")
+    .promise.then((pdfDoc) => {
+      //   console.log(pdfDoc);
+      let pages = pdfDoc._pdfInfo.numPages;
+      for (let i = 1; i < pages; i++) {
+        pdfDoc
+          .getPage(i)
+          .then((page) => {
+            // console.log(page);
+            let pdfCanvas = document.createElement("canvas");
+            let context = pdfCanvas.getContext("2d");
+            let pageViewPort = page.getViewport({ scale: 1 });
+            // console.log(pageViewPort);
+            pdfCanvas.width = pageViewPort.width;
+            pdfCanvas.height = pageViewPort.height;
+            $("#pdfViewerDiv").append(pdfCanvas);
+            page.render({
+              canvasContext: context,
+              viewport: pageViewPort,
+            });
+          })
+          .catch((pageErr) => {
+            console.log(pdfErr);
+          });
+      }
+    })
+    .catch((pdfErr) => {
+      console.log(pdfErr);
+    });
+
+  $("#pdfViewerDiv2").html("");
+  pdfjsLib
+    .getDocument(
+      "https://ascendix.com/wp-content/uploads/2023/06/Listing-Activity-Report.pdf"
+    )
+    .promise.then((pdfDoc) => {
+      //   console.log(pdfDoc);
+      let pages = pdfDoc._pdfInfo.numPages;
+      for (let i = 1; i < pages; i++) {
+        pdfDoc
+          .getPage(i)
+          .then((page) => {
+            // console.log(page);
+            let pdfCanvas = document.createElement("canvas");
+            let context = pdfCanvas.getContext("2d");
+            let pageViewPort = page.getViewport({ scale: 1 });
+            // console.log(pageViewPort);
+            pdfCanvas.width = pageViewPort.width;
+            pdfCanvas.height = pageViewPort.height;
+            $("#pdfViewerDiv2").append(pdfCanvas);
+            page.render({
+              canvasContext: context,
+              viewport: pageViewPort,
+            });
+          })
+          .catch((pageErr) => {
+            console.log(pdfErr);
+          });
+      }
+    })
+    .catch((pdfErr) => {
+      console.log(pdfErr);
+    });
+};
+
+$(function () {
+  initPDFViewer();
+});
+
 //  SMOOTH SCROLL
 const smoothScroll = () => {
   const anchors = document.querySelectorAll('a[href*="#"]');
